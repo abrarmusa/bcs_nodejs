cbcbaef66f390861b16537b404eb6f8c7376e110
abrarmusa <abrar.musa.89@gmail.com>
2016-03-22 18:49:41 -0700

    pushing changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Portfolio' });
+  res.render('index', { title: 'AHASHSHSAHDAHSDHAHSDHSAHDASHHD' });
 });
+
+
+router.get('/dif1', function(req, res, next) {
+  res.render('index', { title: 'AHASHSHSAHDAHSDHAHSDHSAHDASHHD' });
+});
+
+router.get('/sample2', function(req, res, next) {
+  res.render('index', { title: 'sample2' });
+});
+
+router.get('/sample3', function(req, res, next) {
+  res.render('index', { title: 'sample3' });
+});
+
+router.get('/sample4', function(req, res, next) {
+  res.render('index', { title: 'sample4' });
+});
+
+router.post('/postroutes', function(req, res, next){
+	console.log("YAY! Someone is asking for something");
+	console.log(req.body);
+})
+
+
 
 module.exports = router;
